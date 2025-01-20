bef22eaac65f8ee24f9cc6687fe5ee36e926e40f
Admin <Admin@EA611-14>
2025-01-20 20:03:14 +0800

    Add project files.

---
+++ ConsoleAppfizzbuzz/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace activity1
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            for (int i = 0; i <= 100; i++) // start with 0
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
